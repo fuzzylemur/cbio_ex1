362763e90f9e8eba05859e724f81f46a87ded7a5
fuzzylemur <giladam@gmail.com>
2019-11-06 21:34:50 +0200

    Add files via upload

--- seq_align.py
+++ seq_align.py
@@ -1,5 +1,6 @@
 import argparse
 import numpy as np
+
 from itertools import groupby
 
 def fastaread(fasta_name):
@@ -18,6 +19,41 @@ def main():
     parser.add_argument('--align_type', help='Alignment type (e.g. local)', required=True)
     parser.add_argument('--score', help='Score matrix in.tsv format (default is score_matrix.tsv) ', default='score_matrix.tsv')
     command_args = parser.parse_args()
+
+    h1, s = next(fastaread(command_args.seq_a))
+    h2, t = next(fastaread(command_args.seq_b))
+    s = "TTT"
+    t = "AA"
+    n = len(s)
+    m = len(t)
+    score = np.genfromtxt(command_args.score, delimiter='\t')[1:,1:]
+
+    d = {"A":0, "C":1, "G":2, "T":3}
+
+    arr = np.zeros((n,m))
+    path = np.zeros((n,m))
+
+    arr[0, 0] = score[d[s[0]], d[t[0]]]
+    for i in range(1,n):
+        arr[i,0] = arr[i-1,0] + score[4, d[s[i]]]
+
+    for i in range(n):
+        for j in range(1,m):
+            val1 = arr[i-1,j] + score[d[s[i]], 4]
+            val2 = arr[i,j-1] + score[4, d[t[j]]]
+            val3 = arr[i-1,j-1] + score[d[s[i]], d[t[j]]]
+            vals = [val1,val2,val3]
+            print(i, j, vals)
+            arr[i,j] = max(vals)
+            path[i,j] = np.argmax(vals)
+
+    max_score = arr[i,j]
+
+    trace1, trace2 = "",""
+    print(arr)
+    print(path)
+
+
     if command_args.align_type == 'global':
         raise NotImplementedError
     elif command_args.align_type == 'local':
