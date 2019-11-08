f012edba1a06e7955b95cc75012471cf95be4409
giladam <giladam@cs.huji.ac.il>
2019-11-08 14:53:25 +0200

    fixed array indexing

--- seq_align.py
+++ seq_align.py
@@ -20,30 +20,34 @@ def main():
     parser.add_argument('--score', help='Score matrix in.tsv format (default is score_matrix.tsv) ', default='score_matrix.tsv')
     command_args = parser.parse_args()
 
-    h1, s = next(fastaread(command_args.seq_a))
-    h2, t = next(fastaread(command_args.seq_b))
-    s = "TTT"
-    t = "AA"
-    n = len(s)
-    m = len(t)
+    h1, seq1 = next(fastaread(command_args.seq_a))
+    h2, seq2 = next(fastaread(command_args.seq_b))
+    seq1 = "GGCC"
+    seq2 = "GGTTCC"
+    n = len(seq1)
+    m = len(seq2)
     score = np.genfromtxt(command_args.score, delimiter='\t')[1:,1:]
 
     d = {"A":0, "C":1, "G":2, "T":3}
 
-    arr = np.zeros((n,m))
-    path = np.zeros((n,m))
+    arr = np.zeros((n+1,m+1))
+    path = np.zeros((n+1,m+1))
 
-    arr[0, 0] = score[d[s[0]], d[t[0]]]
-    for i in range(1,n):
-        arr[i,0] = arr[i-1,0] + score[4, d[s[i]]]
+    # fill first row and column
+    arr[0, 0] = 0
+    for j in range(1,m+1):
+        arr[0,j] = arr[0,j-1] + score[4, d[seq2[j-1]]]
+    for i in range(1,n+1):
+        arr[i,0] = arr[i-1,0] + score[4, d[seq1[i-1]]]
 
-    for i in range(n):
-        for j in range(1,m):
-            val1 = arr[i-1,j] + score[d[s[i]], 4]
-            val2 = arr[i,j-1] + score[4, d[t[j]]]
-            val3 = arr[i-1,j-1] + score[d[s[i]], d[t[j]]]
-            vals = [val1,val2,val3]
-            print(i, j, vals)
+    # fill rest of array
+    for i in range(1,n+1):
+        for j in range(1,m+1):
+            val1 = arr[i-1,j] + score[d[seq1[i-1]], 4]
+            val2 = arr[i,j-1] + score[4, d[seq2[j-1]]]
+            val3 = arr[i-1,j-1] + score[d[seq1[i-1]], d[seq2[j-1]]]
+            vals = [val1, val2, val3]
+            #print(i, j, vals)
             arr[i,j] = max(vals)
             path[i,j] = np.argmax(vals)
 
